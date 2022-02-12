595300b53efe9d4b7eb90d18486470aacada7148
Narudon Wannaphanom <narudon@magicboxasia.com>
2022-02-13 01:12:28 +0700

    Update Info

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,12 @@ function App() {
       <header className="App-header">
         <p>
           <div className='paragraph main'>Hello, I'm Don.</div>
-          <div className='paragraph sub'>currently CTO at Magic Box Solutions Co.,Ltd.</div>
-          <img src={photo} className='protrait'/>
+          <div className='paragraph sub'>currently as fulltime CTO at <a href='https://www.magicboxsolution.com' className='paragraph sub'>Magic Box Solutions Co.,Ltd.</a></div>
+          <div className='paragraph sub'>And part-time "life coach", 24/7 WFH ={'>'} "Work For Her"</div>
+          <div className='paragraph sub'>Happy wife, Happy life</div>
+          <div className='paragraph sub'>Scroll down to check update,</div>
+          <div className='paragraph sub'>Can't do it, do you?, Yeah not thing update.</div>
+          <img src={photo} className='portrait' alt=''/>
         </p>
       </header>
     </div>
