bb7b8a15378ba7a9d6da1ba92fd8cd36ce9b63ae
Narudon Wannaphanom <narudon.wannaphanom@gmail.com>
2022-02-13 10:27:35 +0700

    update structure

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <nav className="main-navbar">
+          <ul>
+            <li>Narudon Wannaphanom</li>
+            <li>About</li>
+            <li>Blog</li>
+          </ul>
+        </nav>
         <p>
           <div className='paragraph main'>Hello, I'm Don.</div>
           <div className='paragraph sub'>currently as fulltime CTO at <a href='https://www.magicboxsolution.com' className='paragraph sub'>Magic Box Solutions Co.,Ltd.</a></div>
